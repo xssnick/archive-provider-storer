416d25ebf7928153a643c4c6710f3a6e59089360
Oleg Baranov <xssnick8@gmail.com>
2025-06-25 12:29:52 +0400

    Init

---
+++ cmd/service/main.go
@@ -0,0 +1,558 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/ed25519"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/pterm/pterm"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/xssnick/archive-manager/pkg/index"
+	"github.com/xssnick/tonutils-go/address"
+	"github.com/xssnick/tonutils-go/adnl"
+	"github.com/xssnick/tonutils-go/adnl/dht"
+	"github.com/xssnick/tonutils-go/liteclient"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/ton"
+	"github.com/xssnick/tonutils-go/ton/wallet"
+	"github.com/xssnick/tonutils-go/tvm/cell"
+	"github.com/xssnick/tonutils-storage-provider/pkg/contract"
+	"github.com/xssnick/tonutils-storage-provider/pkg/storage"
+	"github.com/xssnick/tonutils-storage-provider/pkg/transport"
+	"github.com/xssnick/tonutils-storage/provider"
+	"math"
+	"math/big"
+	"math/rand"
+	"net/http"
+	"os"
+	"sync/atomic"
+	"time"
+)
+
+type Provider struct {
+	ID            []byte
+	WaitingForBag bool
+}
+
+type MTPResponse struct {
+	Providers []struct {
+		PubKey string `json:"pubkey"`
+	} `json:"providers"`
+}
+
+var minBalanceStr = flag.String("min-contract-balance", "0.3", "Minimum contract balance to topup")
+var maxBalanceStr = flag.String("contract-balance", "1", "Contract balance to maintain")
+var gasFeeAddStr = flag.String("gas-fee", "0.05", "Additional gas fee per tx")
+var maxSpanAllowed = flag.Int("max-span", 86400*14, "Maximum span allowed sec")
+var maxPerMBStr = flag.String("max-per-mb", "0.00015", "Maximum price per MB")
+var minRewardToVerifyStr = flag.String("max-reward-calc", "0.055", "Max reward after which we will calculate by per MB rate")
+var walletKeyBase = flag.String("wallet-key", "", "Wallet private key in Base64 (32 bytes)")
+var networkConfigUrl = flag.String("network-config-url", "https://ton.org/global.config.json", "TON Network configuration URL")
+var storageUrl = flag.String("storage-url", "http://127.0.0.1:8081", "Tonutils Storage server URL")
+var storageLogin = flag.String("storage-login", "1", "Tonutils Storage server login")
+var storagePassword = flag.String("storage-password", "1", "Tonutils Storage server password")
+var indexUrl = flag.String("index-url", "https://archival-dump.ton.org/index/mainnet.json", "Bags index URL")
+var limitBags = flag.Int("limit-bags", 10, "Limit for bags to process (0 = all)")
+var limitProviders = flag.Int("limit-providers", 100, "Limit for providers filter request (all fetched within single request)")
+
+var minBalance, maxBalance, gasFeeAdd, maxPerMB, minRewardToVerify tlb.Coins
+
+func main() {
+	flag.Parse()
+
+	minBalance = tlb.MustFromTON(*minBalanceStr)
+	maxBalance = tlb.MustFromTON(*maxBalanceStr)
+	gasFeeAdd = tlb.MustFromTON(*gasFeeAddStr)
+	maxPerMB = tlb.MustFromTON(*maxPerMBStr)
+	minRewardToVerify = tlb.MustFromTON(*minRewardToVerifyStr)
+
+	var walletKey, err = base64.StdEncoding.DecodeString(*walletKeyBase)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to decode wallet key")
+		return
+	}
+
+	log.Logger = zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Logger().Level(zerolog.DebugLevel)
+
+	lsCfg, err := liteclient.GetConfigFromUrl(context.Background(), *networkConfigUrl)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get lite client config")
+		return
+	}
+
+	lsClient := liteclient.NewConnectionPool()
+	if err := lsClient.AddConnectionsFromConfig(context.Background(), lsCfg); err != nil {
+		pterm.Error.Println("Failed to init LS client:", err.Error())
+		os.Exit(1)
+	}
+
+	// initialize ton api lite connection wrapper with full proof checks
+	api := ton.NewAPIClient(lsClient, ton.ProofCheckPolicyFast).WithRetry()
+
+	wl, err := initWallet(api, ed25519.NewKeyFromSeed(walletKey))
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to init wallet")
+		return
+	}
+	log.Info().Str("addr", wl.WalletAddress().String()).Msg("wallet initialized")
+
+	_, dhtKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to generate dht key")
+		return
+	}
+
+	_, prvKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to generate provider key")
+		return
+	}
+
+	dhtGate := adnl.NewGateway(dhtKey)
+	if err = dhtGate.StartClient(); err != nil {
+		log.Fatal().Err(err).Msg("failed to init dht adnl gateway")
+		return
+	}
+
+	dhtClient, err := dht.NewClientFromConfig(dhtGate, lsCfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to init dht client")
+		return
+	}
+
+	providerGate := adnl.NewGateway(prvKey)
+	if err = providerGate.StartClient(); err != nil {
+		log.Fatal().Err(err).Msg("failed to init provider adnl gateway")
+		return
+	}
+
+	pClient := transport.NewClient(providerGate, dhtClient)
+
+	cl := storage.NewClient(*storageUrl, "./provider-store", nil, &storage.Credentials{
+		Login:    *storageLogin,
+		Password: *storagePassword,
+	})
+
+	var fromBlock uint32 = 0
+	for {
+		fromBlock, err = doLoop(wl, cl, pClient, api, *indexUrl, "https://mytonprovider.org/api/v1/providers/search", fromBlock)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to do loop")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		log.Info().Msg("loop completed, sleeping 15s")
+
+		time.Sleep(15 * time.Second)
+	}
+}
+
+var addedProviders = make(map[string]time.Time)
+
+func doLoop(wl *wallet.Wallet, storageClient *storage.Client, providerClient *transport.Client, api ton.APIClientWrapped, idxUrl, providersUrl string, fromBlock uint32) (uint32, error) {
+	hCli := http.Client{Timeout: 10 * time.Second}
+	resp, err := hCli.Get(idxUrl)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get index: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var idx index.Index
+	if err = json.NewDecoder(resp.Body).Decode(&idx); err != nil {
+		return 0, fmt.Errorf("failed to decode index: %w", err)
+	}
+
+	log.Info().Msg("index downloaded")
+
+	var bags []string
+	for _, b := range idx.Blocks {
+		bags = append(bags, b.Bag)
+	}
+	for _, state := range idx.States {
+		bags = append(bags, state.Bag)
+	}
+
+	var providers []*Provider
+	var details []*storage.BagDetailed
+
+	log.Info().Msg("loading bags")
+
+	if len(bags) > *limitBags && *limitBags > 0 {
+		bags = bags[:*limitBags]
+	}
+
+next:
+	for _, b := range bags {
+		bag, err := hex.DecodeString(b)
+		if err != nil {
+			log.Error().Err(err).Str("bag", b).Msg("failed to decode bag")
+			continue
+		}
+
+		log.Debug().Msgf("loading bag %s", b)
+
+		for {
+			detail, err := storageClient.GetBag(context.Background(), bag)
+			if err != nil {
+				if errors.Is(err, storage.ErrNotFound) {
+					log.Info().Msgf("downloading header of %s", b)
+					if err = storageClient.StartDownload(context.Background(), bag, false); err != nil {
+						log.Error().Err(err).Str("bag", b).Msg("failed to download header")
+						continue next
+					}
+				} else {
+					log.Error().Err(err).Str("bag", b).Msg("failed to get bag")
+					continue next
+				}
+			}
+
+			if detail == nil || !detail.HeaderLoaded {
+				log.Debug().Msgf("waiting bag %s", b)
+
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+
+			details = append(details, detail)
+			break
+		}
+	}
+
+	log.Info().Msg("fetching providers")
+
+	respProviders, err := hCli.Post(providersUrl, "application/json", bytes.NewBuffer([]byte(`{
+		"filter": {},
+		"sort": {
+			"column": "rating",
+			"order": "desc"
+		},
+		"exact": [],
+		"limit": `+fmt.Sprint(*limitProviders)+`,
+		"offset": 0
+	}`)))
+	if err != nil {
+		log.Error().Err(err).Msg("failed to fetch providers")
+		return 0, err
+	}
+	defer respProviders.Body.Close()
+
+	var providersList MTPResponse
+	if err = json.NewDecoder(respProviders.Body).Decode(&providersList); err != nil {
+		return 0, fmt.Errorf("failed to decode index: %w", err)
+	}
+
+	for _, p := range providersList.Providers {
+		key, err := hex.DecodeString(p.PubKey)
+		if err != nil {
+			continue
+		}
+
+		providers = append(providers, &Provider{
+			ID: key,
+		})
+	}
+
+	log.Info().Msg("checking bags")
+
+	var messages []*wallet.Message
+	for _, dt := range details {
+		if len(messages) >= 100 {
+			log.Info().Msg("100 messages reached, skip other bags processing for now")
+			break
+		}
+
+		id, err := hex.DecodeString(dt.BagID)
+		if err != nil {
+			log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to decode bag id")
+			continue
+		}
+
+		mHash, err := hex.DecodeString(dt.MerkleHash)
+		if err != nil {
+			log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to decode merkle hash")
+			continue
+		}
+
+		addr, _, _, err := contract.PrepareV1DeployData(id, mHash, dt.BagSize, dt.PieceSize, wl.WalletAddress(), nil)
+		if err != nil {
+			log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to calc contract address")
+			continue
+		}
+
+		master, err := api.WaitForBlock(fromBlock).CurrentMasterchainInfo(context.Background())
+		if err != nil {
+			log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to get masterchain info")
+			continue
+		}
+
+		curProviders, balance, err := contract.GetProvidersV1(context.Background(), api, master, addr)
+		if err != nil && !errors.Is(err, contract.ErrNotDeployed) {
+			log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to calc contract address")
+			continue
+		}
+
+		var validProviders []contract.ProviderDataV1
+		for _, prv := range curProviders {
+			existsInList := false
+			for _, p := range providers {
+				if bytes.Equal(prv.Key, p.ID) {
+					existsInList = true
+					break
+				}
+			}
+
+			toProof := uint64(rand.Int()) % dt.BagSize
+
+			var suspect bool
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+			stResp, err := providerClient.RequestStorageInfo(ctx, prv.Key, addr, toProof)
+			cancel()
+			if err != nil {
+				suspect = true
+				log.Warn().Err(err).Str("bag", dt.BagID).Hex("provider", prv.Key).Msg("failed to get storage info")
+			} else if stResp.Status != "active" || verifyStorageProof(id, dt.BagSize, stResp.Proof, toProof, dt.PieceSize) {
+				suspect = true
+				log.Debug().Str("bag", dt.BagID).Hex("provider", prv.Key).Str("status", stResp.Status).Str("reason", stResp.Reason).Msg("waiting for bag confirmation")
+			}
+
+			if suspect {
+				// still downloading or not responding, remove provider from list for this loop, to not give him a new bag
+				for _, pv := range providers {
+					if bytes.Equal(pv.ID, prv.Key) {
+						pv.WaitingForBag = true
+						break
+					}
+				}
+
+				maxDelay := time.Duration(prv.MaxSpan*2) * time.Second
+				if prv.LastProofAt.Add(maxDelay).Before(time.Now()) && time.Since(addedProviders[base64.StdEncoding.EncodeToString(prv.Key)+"_"+dt.BagID]) > 2*time.Hour {
+					// no proofs for too long, and not in list, removing it
+					if !existsInList {
+						log.Warn().Str("bag", dt.BagID).Hex("provider", prv.Key).Msg("last proof too long ago, and provider not in list anymore, removing")
+						continue
+					}
+					log.Warn().Str("bag", dt.BagID).Hex("provider", prv.Key).Msg("last proof too long ago, but provider in trusted list, not removing")
+				}
+			} else {
+				log.Info().Str("bag", dt.BagID).Hex("provider", prv.Key).Msg("storage confirmed")
+			}
+
+			validProviders = append(validProviders, prv)
+		}
+
+		if len(validProviders) < 3 {
+			var newProviders []contract.ProviderV1
+			for _, prv := range validProviders {
+				// add prev valid providers
+				newProviders = append(newProviders, contract.ProviderV1{
+					Address:       address.NewAddress(0, 0, prv.Key),
+					MaxSpan:       prv.MaxSpan,
+					PricePerMBDay: prv.RatePerMB,
+				})
+			}
+
+			var providerUpdated bool
+		nextProvider:
+			for _, prv := range providers {
+				if len(newProviders) == 3 {
+					break
+				}
+				if prv.WaitingForBag {
+					log.Debug().Str("bag", dt.BagID).Hex("provider", prv.ID).Msg("provider is waiting for another bag confirmation")
+					continue
+				}
+
+				for _, newProvider := range newProviders {
+					if bytes.Equal(prv.ID, newProvider.Address.Data()) {
+						// already exists
+						continue nextProvider
+					}
+				}
+
+				rates, err := providerClient.GetStorageRates(context.Background(), prv.ID, dt.BagSize)
+				if err != nil {
+					log.Error().Err(err).Str("bag", dt.BagID).Hex("provider", prv.ID).Msg("failed to get rates")
+					continue
+				}
+
+				if !rates.Available || rates.SpaceAvailableMB < dt.BagSize>>20 {
+					log.Debug().Str("bag", dt.BagID).Hex("provider", prv.ID).Msg("not available")
+					continue
+				}
+
+				perMB := tlb.FromNanoTON(new(big.Int).SetBytes(rates.RatePerMBDay))
+				offer := provider.CalculateBestProviderOffer(&provider.ProviderRates{
+					Available:        rates.Available,
+					RatePerMBDay:     perMB,
+					MinBounty:        tlb.FromNanoTON(new(big.Int).SetBytes(rates.MinBounty)),
+					SpaceAvailableMB: rates.SpaceAvailableMB,
+					MinSpan:          rates.MinSpan,
+					MaxSpan:          rates.MaxSpan,
+					Size:             dt.BagSize,
+				})
+
+				if offer.Span > uint32(*maxSpanAllowed) {
+					log.Debug().Str("bag", dt.BagID).Hex("provider", prv.ID).Msg("span too high, skip")
+					continue
+				}
+
+				if offer.PerProofNano.Cmp(minRewardToVerify.Nano()) > 0 {
+					// not typical price, check per mb
+					if offer.RatePerMBNano.Cmp(maxPerMB.Nano()) > 0 {
+						log.Debug().Str("bag", dt.BagID).Hex("provider", prv.ID).Msg("rate too high, skip")
+						continue
+					}
+				}
+
+				log.Info().Str("bag", dt.BagID).Hex("provider", prv.ID).Str("per_mb_day", perMB.String()).Msg("adding new provider")
+
+				newProviders = append(newProviders, contract.ProviderV1{
+					Address:       address.NewAddress(0, 0, prv.ID),
+					MaxSpan:       offer.Span,
+					PricePerMBDay: tlb.FromNanoTON(offer.RatePerMBNano),
+				})
+
+				// remove provider for this loop, to not give him a new bag until he downloads it
+				prv.WaitingForBag = true
+				addedProviders[base64.StdEncoding.EncodeToString(prv.ID)+"_"+dt.BagID] = time.Now()
+
+				providerUpdated = true
+			}
+
+			if providerUpdated {
+				_, si, body, err := contract.PrepareV1DeployData(id, mHash, dt.BagSize, dt.PieceSize, wl.WalletAddress(), newProviders)
+				if err != nil {
+					log.Error().Err(err).Str("bag", dt.BagID).Msg("failed to calc deploy data")
+					continue
+				}
+
+				amt := gasFeeAdd
+
+				if balance.LessThan(&maxBalance) {
+					amt = *amt.MustAdd(maxBalance.MustSub(&balance))
+				}
+
+				messages = append(messages, &wallet.Message{
+					Mode: 1 + 2,
+					InternalMessage: &tlb.InternalMessage{
+						IHRDisabled: true,
+						Bounce:      false,
+						DstAddr:     addr,
+						Amount:      amt,
+						StateInit:   si,
+						Body:        body,
+					},
+				})
+				continue
+			}
+		}
+
+		if len(validProviders) > 0 && balance.LessThan(&minBalance) {
+			topupAmt := maxBalance.MustSub(&balance)
+
+			log.Info().Str("addr", addr.String()).Int("providers", len(validProviders)).Str("bag", dt.BagID).Str("topup", topupAmt.String()).Msg("adding message")
+
+			messages = append(messages, &wallet.Message{
+				Mode: 1 + 2,
+				InternalMessage: &tlb.InternalMessage{
+					IHRDisabled: true,
+					Bounce:      true, // expected to be deployed
+					DstAddr:     addr,
+					Amount:      *topupAmt,
+				},
+			})
+		}
+	}
+
+	if len(messages) > 0 {
+		tx, block, err := wl.SendManyWaitTransaction(context.Background(), messages)
+		if err != nil {
+			return 0, fmt.Errorf("failed to send messages: %w", err)
+		}
+		fromBlock = block.SeqNo
+
+		log.Info().Str("hash", base64.StdEncoding.EncodeToString(tx.Hash)).Int("messages", len(messages)).Msg("executed transaction")
+	}
+
+	return fromBlock, nil
+}
+
+func initWallet(apiClient wallet.TonAPI, key ed25519.PrivateKey) (*wallet.Wallet, error) {
+	walletAbstractSeqno := uint32(0)
+	w, err := wallet.FromPrivateKey(apiClient, key, wallet.ConfigHighloadV3{
+		MessageTTL: 3*60 + 30,
+		MessageBuilder: func(ctx context.Context, subWalletId uint32) (id uint32, createdAt int64, err error) {
+			createdAt = time.Now().UTC().Unix() - 30 // something older than last master block, to pass through LS external's time validation
+			// TODO: store seqno in db
+			id = uint32((createdAt%(3*60+30))<<15) | atomic.AddUint32(&walletAbstractSeqno, 1)%(1<<15)
+			return
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+func verifyStorageProof(hash []byte, sz uint64, proofBytes []byte, proofByte uint64, pieceSz uint32) bool {
+	proof, err := cell.FromBOC(proofBytes)
+	if err == nil {
+		if proofData, err := cell.UnwrapProof(proof, hash); err == nil {
+			piece := uint32(proofByte / uint64(pieceSz))
+			pieces := uint32(sz / uint64(pieceSz))
+
+			if err = checkProofBranch(proofData, piece, pieces); err == nil {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func checkProofBranch(proof *cell.Cell, piece, piecesNum uint32) error {
+	if piece >= piecesNum {
+		return fmt.Errorf("piece is out of range %d/%d", piece, piecesNum)
+	}
+
+	tree := proof.BeginParse()
+
+	// calc tree depth
+	depth := int(math.Log2(float64(piecesNum)))
+	if piecesNum > uint32(math.Pow(2, float64(depth))) {
+		// add 1 if pieces num is not exact log2
+		depth++
+	}
+
+	// check bits from left to right and load branches
+	for i := depth - 1; i >= 0; i-- {
+		isLeft := piece&(1<<i) == 0
+
+		b, err := tree.LoadRef()
+		if err != nil {
+			return err
+		}
+
+		if isLeft {
+			tree = b
+			continue
+		}
+
+		// we need right branch
+		tree, err = tree.LoadRef()
+		if err != nil {
+			return err
+		}
+	}
+
+	if tree.BitsLeft() != 256 {
+		return fmt.Errorf("incorrect branch")
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/xssnick/archive-provider-storer
+
+go 1.24
+
+require (
+	github.com/pterm/pterm v0.12.80
+	github.com/rs/zerolog v1.34.0
+	github.com/xssnick/archive-manager v0.0.0-20250612065758-6a0014f3a787
+	github.com/xssnick/tonutils-go v1.13.0
+	github.com/xssnick/tonutils-storage v1.1.1
+	github.com/xssnick/tonutils-storage-provider v0.3.7
+)
+
+require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
+	github.com/containerd/console v1.0.4 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/kevinms/leakybucket-go v0.0.0-20200115003610-082473db97ca // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20230904125328-1f23a7beb09a // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sigurn/crc16 v0.0.0-20240131213347-83fcde1e29d1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	github.com/xssnick/raptorq v1.0.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
